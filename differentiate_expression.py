a3b189fffcaad11cd72ed2e39d4b9fbb65e8a317
TechIdeaFactory <112824975+TechIdeaFactory@users.noreply.github.com>
2022-11-05 16:51:17 +0000

    Add files via upload
    Differentiate expression.

---
+++ differentiate_expression.py
@@ -0,0 +1,24 @@
+import sympy as sy
+
+
+def x_expr_diff(
+    x_expr: str, x_value: float
+):
+    expr = sy.sympify(x_expr.lower())
+    expr_diff = sy.diff(expr, "x")
+    x = sy.symbols("x")
+    return (
+        str(expr_diff),
+        expr_diff.subs(x, x_value),
+    )
+
+
+diff, val = x_expr_diff("x**2", 1.00)
+print("Derivative = " + diff)
+print("Derivative Value = " + str(val))
+
+diff, val = x_expr_diff(
+    "cos(x**2)", 0.0
+)
+print("Derivative = " + diff)
+print("Derivative Value = " + str(val))
